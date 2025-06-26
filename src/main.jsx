361cc113bb234d7b41521f9d71b8cbeb0f999d97
Nishanthini2411 <nishanishanthini189@gmail.com>
2025-06-26 02:26:50 -0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Navbar from './navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Btn from './Button.jsx'
+import Table from './Table.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+
+    <Navbar/>
+    <Table/>
+
+        <Btn/>
+
+  </StrictMode>,
+)
---
+++ src/navbar.jsx
@@ -0,0 +1,41 @@
+function Navbar(){
+
+    return(
+        <>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        <div>
+            <h1>Hi Friends!</h1>
+        </div>
+        My name is Nishanthini. I'm from Kilinochchi. I'm 20 years old. My Father name is Kularasa. 
+        My Mother name is Santhinithevi. I have a brother and two sisters.
+        I finished my Advanced Level and currently I'm studying fullstruck development at DonBosco, Kilinochchi.
+        My hobbies are music and travelling I want to become a It Professional.
+        My favourite quote is Success is getting what you want, happiness is wanting what you get.
+        
+        </>
+    )
+}
+export default Navbar
